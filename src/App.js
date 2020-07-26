157c0d9b0599b9745b37cc60ce86fef16da1d343
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-26 23:46:46 +0530

    basic class component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+//class based component
+
+class App extends React.Component{
+    render(){
+      return (
+             
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         This is a single page application
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This is a class based component</p>
       </header>
-    </div>
-  );
+
+      )
+    }
 }
+
+
+
+
+
+//Functional based component
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//          This is a single page application
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;

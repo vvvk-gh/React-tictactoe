493b6719f3b6e207363624fa9a6f712d8f79c5f1
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-27 00:49:56 +0530

    square component basic done

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,3 +1,11 @@
-//Compoment File name should be always capitals just like Square.js , Game.js , Board.js
+//Compoment , comp file names : The first letter should be always capitals just like Square.js , Game.js , Board.js
 
+import React from "react"
 
+export default function Square() {
+  return(
+    <button className="square">
+        
+    </button>
+  )
+}

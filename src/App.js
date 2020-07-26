363b0c1d03d6428e2a0ee8aabf96c7a93cb3f35a
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-26 20:38:12 +0530

    functional based compoment :App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello world!
+         This is a single page application
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//render(what to render , where to render)
+ReactDOM.render(<App />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change

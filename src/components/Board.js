ee8d53e48af8a6770bb51b96232af68458ea5f9d
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-27 01:09:09 +0530

    board component done

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,1 +1,29 @@
 //Compoment File name should be always capitals just like Square.js , Game.js , Board.js
+
+import React from 'react'
+import Square from './Square'
+
+export default function Board() {
+       return (
+        <div>
+            {/* We cant send mutiple div so we wrapped all 3 div in one parent div */}
+            <div className="board-row">
+               {/*  Jsx sync */}
+               <Square/> 
+               <Square/>
+               <Square/>
+           </div>
+           <div className="board-row">
+               <Square/>
+               <Square/>
+               <Square/>
+           </div>
+           <div className="board-row">
+               <Square/>
+               <Square/>
+               <Square/>
+           </div>
+        </div>
+           
+       )
+}

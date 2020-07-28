8b6f017000c6e325451795f4a94d57bf799ea02f
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-29 01:27:39 +0530

    Winning combinations and winning logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -14,7 +14,7 @@ export default class Game extends React.Component {
     }
 
 
-    handleClick = (i) =>{
+   handleClick = (i) =>{
        const history = this.state.history
        const current = history[history.length - 1]
        const squares = current.squares
@@ -41,4 +41,28 @@ export default class Game extends React.Component {
         )
     }
     
+calculateWinner =  (squares) => {
+    //possible winning positions  
+    const possibilites = [
+      [0,1,2], //row
+      [3,4,5], //row2
+      [6,7,8], //row3
+      [0,3,6], //col1
+      [1,4,7], //col2
+      [2,5,8], //col3
+      [0,4,8], //diagonal
+      [2,4,6], //diagonal
+      ]
+
+       for(let i=0; i < possibilites.length; i++){
+           //destrcuting the array
+           //possibilites[i] = [x,y,z] from above array
+           //a =x , b = y , c=z
+            const [a,b,c] = possibilites[i]
+            if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+                    return squares[a]
+            }
+        }
+        return null
+    }
 }
